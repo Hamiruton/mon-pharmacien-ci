099ed8d7c2933f67a408ae82790766445216f1c5
Hamiruton <axelhamilton02@gmail.com>
2023-02-11 13:13:55 +0000

    Define LeadSearchBar component

---
+++ vue-app-v1/src/components/LeadSearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-text-field
+        label="Recherche médicament"
+        variant="solo"
+        prepend-inner-icon="mdi-magnify"
+    ></v-text-field>
+</template>
+
+<script>
+
+</script>
