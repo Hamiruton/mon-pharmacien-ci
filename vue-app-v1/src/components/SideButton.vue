6893534883106a23d1886501aab4a47c6ac432a2
Hamiruton <axelhamilton02@gmail.com>
2023-02-11 13:13:32 +0000

    Define SideBar component, a set of buttons

---
+++ vue-app-v1/src/components/SideButton.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-btn
+                :to="{ name: 'home' }"
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour retourner au menu"
+                class="w-100"
+            >HOME</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour envoyer ton ordonnane/bon d'assurance"
+                class="w-100"
+            >ORDONNANCE</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour voir les pharmacies (de garde)"
+                class="w-100"
+            >PHARMACIE</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour voir l'historique de tes activités"
+                class="w-100"
+            >HISTORIQUE</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+                :to="{ name: 'settings' }"
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour voir les paramètres"
+                class="w-100"
+            >PARAMÈTRES</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn
+                :to="{ name: 'about' }"
+                color="teal-lighten-3"
+                size="x-large"
+                rounded="lg"
+                title="Cliquer pour voir about"
+                class="w-100"
+            >ABOUT</v-btn>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+</script>
