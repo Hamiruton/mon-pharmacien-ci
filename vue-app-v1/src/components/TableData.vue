9c7b7430d1feaa85db458188c9944ed58c80ea6b
Hamiruton <axelhamilton02@gmail.com>
2023-02-12 09:03:07 +0000

    Table data component created to to display the data in table form in all other components

---
+++ vue-app-v1/src/components/TableData.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-table fixed-header height="300px" class="table">
+        <thead>
+            <tr>
+                <th class="text-center" v-for="obj, index in Object.keys(listPharmacy[0])" :key="index">{{ obj }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item,n in listPharmacy" :key="n">
+                <td v-for="nb, key in Object.keys(item)" :key="key">{{ item[nb] }}</td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<script>
+    export default {
+        name: "TableData",
+        props: {
+            listPharmacy: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style>
+    .table {
+        background: #fafafa;
+        text-align: center;
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    th {
+        background-color: #ba68c8;
+        color: #fafafa;
+        text-transform: uppercase;
+    }
+
+    tr:nth-child(odd) {
+        background-color: #eeeeee;
+    }
+</style>
