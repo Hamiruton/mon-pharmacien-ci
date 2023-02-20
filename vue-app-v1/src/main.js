0798219f94579c53efe70a94135477d5010c3155
Hamiruton <axelhamilton02@gmail.com>
2023-02-20 17:09:38 +0000

    Add Layout officine

--- vue-app-v1/src/main.js
+++ vue-app-v1/src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import LayoutDefault from './layouts/LayoutDefault.vue'
+import LayoutOfficine from './layouts/LayoutOfficine'
 
 loadFonts()
 
@@ -15,4 +16,5 @@ createApp(App)
   .use(store)
   .use(vuetify)
   .component('LayoutDefault', LayoutDefault)
+  .component('LayoutOfficine', LayoutOfficine)
   .mount('#app')
