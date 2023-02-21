93e61dba6614b11a6edf2ddc50f9c63166560924
Hamiruton <axelhamilton02@gmail.com>
2023-02-21 18:50:54 +0000

    Create layout for officine

---
+++ vue-app-v1/src/layouts/LayoutOfficine.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app>
+        <AppBarOfficine />
+        <v-main class="bg-grey-lighten-3">
+            <v-container class="px-6 py-9" fluid>
+                <v-row>
+                    <v-col>
+                        <v-sheet></v-sheet>
+                    </v-col>
+                    <v-col cols="10">
+                        <LeadSearchBar />
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="2">
+                        <SideBtnOfficine />
+                    </v-col>
+                    <v-col>
+                        <slot></slot>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import AppBarOfficine from '@/components/AppBarOfficine.vue';
+    import SideBtnOfficine from '@/components/SideBtnOfficine.vue';
+    import LeadSearchBar from '@/components/LeadSearchBar.vue';
+    export default {
+        name: "LayoutOfficine",
+        components: { AppBarOfficine, SideBtnOfficine, LeadSearchBar }
+    }
+</script>
+
