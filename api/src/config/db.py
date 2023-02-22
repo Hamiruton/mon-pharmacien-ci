54b669c42785c7389732819b2f8be72876deaed5
Hamiruton <axelhamilton02@gmail.com>
2023-02-22 09:57:45 +0000

    db conf update

--- api/src/config/db.py
+++ api/src/config/db.py
@@ -1,5 +1,16 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient('localhost', 27017)
+def get_database():
+    CONNECTION_STRING = os.getenv('FLASK_MONGO_URI')
+    
+    client = MongoClient(CONNECTION_STRING)
+    db = client['pi2023db']
 
-db = client.pi2023db
+    if len(client.list_database_names()):
+        print('DB up')
+        return db
+
+
+if __name__ == "__main__":
+    get_database()
