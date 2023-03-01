a8d8c2a1e84cc43e6d2b057cef2f71221b749b6d
Hamiruton <axelhamilton02@gmail.com>
2023-03-01 18:23:35 +0000

    Basic Handle prescription

--- vue-app-v1/src/views/officine/HandleStock.vue
+++ vue-app-v1/src/views/officine/HandleStock.vue
@@ -1,6 +1,9 @@
 <template>
     <LayoutOfficine>
-        <h1>Handle stock</h1>
+        <v-sheet min-height="70vh" rounded="lg" class="pa-16 grid" elevation="5">
+            <h1 class="leadTitle">gestion du stock de l'officine</h1>
+
+        </v-sheet>
     </LayoutOfficine>
 </template>
 
@@ -9,3 +12,16 @@
         name: "HandleStock",
     }
 </script>
+
+<style scoped>
+    .leadTitle {
+        text-transform: uppercase;
+        text-align: center;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat();
+    }
+</style>
