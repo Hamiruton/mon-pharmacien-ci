fbe3e9aa38cb23da586be860c9e199a0ff824f75
Hamiruton <axelhamilton02@gmail.com>
2023-03-01 18:19:44 +0000

    Update only_patient to token_required

--- api/src/auth.py
+++ api/src/auth.py
@@ -5,27 +5,29 @@ from functools import wraps
 from src.models.models import auth_by_id
 
 
-def only_patient(func):
-    @wraps(func)
-    def decorator(*args, **kwargs):
-        token = None
-        secret_key = os.getenv('FLASK_SECRET_KEY')
-        if 'Authorization' in request.headers:
-            _ = request.headers['Authorization']
-            token = _.split(' ')[1]
-            if token == '':
-                return make_response(jsonify({"message": "Aucun token fourni"}), 401)
+def token_required(collection):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            token = None
+            secret_key = os.getenv('FLASK_SECRET_KEY')
+            if 'Authorization' in request.headers:
+                _ = request.headers['Authorization']
+                token = _.split(' ')[1]
+                if token == '':
+                    return make_response(jsonify({"message": "Aucun token fourni"}), 401)
 
-        try:
-            data = jwt.decode(token, secret_key, algorithms=['HS256'])
-            is_exist = auth_by_id('users', data['public_id'])
-        except Exception as e:
-            print(e)
-            return make_response(jsonify({"message": "Vous ne pouvez pas accéder à cette ressource !!!"}), 401)
+            try:
+                data = jwt.decode(token, secret_key, algorithms=['HS256'])
+                is_exist = auth_by_id(collection, data['public_id'])
+            except Exception as e:
+                print(e)
+                return make_response(jsonify({"message": "Vous ne pouvez pas accéder à cette ressource !!!"}), 401)
+            
+            if not is_exist:
+                return make_response(jsonify({"message": "Vous ne pouvez pas accéder à cette ressource !!!"}), 401)
+            
+            return func(*args, **kwargs)
         
-        if not is_exist:
-            return make_response(jsonify({"message": "Vous ne pouvez pas accéder à cette ressource !!!"}), 401)
-        
-        return func(*args, **kwargs)
-    
+        return wrapper
     return decorator
