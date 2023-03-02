f3eaeed62934d1ff664b39bb23bfda9e9daa96c5
Hamiruton <axelhamilton02@gmail.com>
2023-03-02 13:41:25 +0000

    Define Handle prescription interface

--- vue-app-v1/src/views/officine/HandlePrescrip.vue
+++ vue-app-v1/src/views/officine/HandlePrescrip.vue
@@ -1,6 +1,25 @@
 <template>
     <LayoutOfficine>
-        <h1>Handle Presciption</h1>
+        <v-sheet min-height="70vh" rounded="lg" class="pa-16" elevation="5">
+            <h1 class="leadTitle">gestion du stock de l'officine</h1>
+            <v-sheet min-height="50vh" class="grid mt-16">
+                <v-col>
+                    <v-btn class="w-100 h-50 text-h6" rounded="lg" color="teal-lighten-3">
+                        ORDONNANCES REÇUES
+                    </v-btn>
+                </v-col>
+                <v-col>
+                    <v-btn class="w-100 h-50 text-h6" rounded="lg" color="teal-lighten-3">
+                        ENREGISTRER ORDONNANCE
+                    </v-btn>
+                </v-col>
+                <v-col>
+                    <v-btn class="w-100 h-50 text-h6" rounded="lg" color="teal-lighten-3">
+                        HISTORIQUE ORDONNANCES TRAITÉES
+                    </v-btn>
+                </v-col>
+            </v-sheet>
+        </v-sheet>
     </LayoutOfficine>
 </template>
 
@@ -9,3 +28,15 @@
         name: "HandlePrescip",
     }
 </script>
+
+<style scoped>
+    .leadTitle {
+        text-transform: uppercase;
+        text-align: center;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+</style>
