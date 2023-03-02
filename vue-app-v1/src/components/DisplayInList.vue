f5c1b8703a1e1c36aba441fe1e2468e8ac3d01db
Hamiruton <axelhamilton02@gmail.com>
2023-03-02 15:45:47 +0000

    create compoent that displays info in list

---
+++ vue-app-v1/src/components/DisplayInList.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card
+        class="mx-auto card-personalized mb-3">
+        <v-card-actions>
+            <v-card-title class="titleList">
+                {{ content }}
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <slot name="actions"></slot>
+        </v-card-actions>
+            
+        
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "DisplayInList",
+        props: ["content"],
+    }
+</script>
+
+<style scoped>
+    .card-personalized {
+        width: 90%;
+        background-color: #eeeeeeff;
+    }
+
+    .titleList {
+        text-transform: uppercase;
+    }
+</style>
