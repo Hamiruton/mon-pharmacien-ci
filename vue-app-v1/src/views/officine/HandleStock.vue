20952b3d401a207de05105ebc98d3a04c7eba531
Hamiruton <axelhamilton02@gmail.com>
2023-03-02 13:07:09 +0000

    Define drugs categories view

--- vue-app-v1/src/views/officine/HandleStock.vue
+++ vue-app-v1/src/views/officine/HandleStock.vue
@@ -1,8 +1,14 @@
 <template>
     <LayoutOfficine>
-        <v-sheet min-height="70vh" rounded="lg" class="pa-16 grid" elevation="5">
+        <v-sheet min-height="70vh" rounded="lg" class="pa-16" elevation="5">
             <h1 class="leadTitle">gestion du stock de l'officine</h1>
-
+            <v-sheet min-height="50vh" class="grid">
+                <v-col v-for="cat, index in categoryDrug" :key="index">
+                    <v-btn class="w-100 h-100 text-h6" rounded="lg" color="teal-lighten-3">
+                        <span class="cat-btn">{{ cat }}</span>
+                    </v-btn>
+                </v-col>
+            </v-sheet>
         </v-sheet>
     </LayoutOfficine>
 </template>
@@ -10,6 +16,22 @@
 <script>
     export default {
         name: "HandleStock",
+        data() {
+            return {
+                categoryDrug: [
+                    "antipaludique",
+                    "grippe",
+                    "antibiotique",
+                    "antifongique",
+                    "duriétique",
+                    "antiarythmiques",
+                    "antiémétiques",
+                    "antigènes",
+                    "micro-immunothérapie"
+                ]
+            }
+        },
+        
     }
 </script>
 
@@ -21,7 +43,12 @@
 
     .grid {
         display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        grid-template-rows: repeat();
+        /*overflow-y: scroll;*/
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    .cat-btn {
+        text-transform: uppercase;
+        font-weight: bold;
     }
 </style>
