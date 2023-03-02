3471cf5ce523f3d01d567139efba66109bc7095c
Hamiruton <axelhamilton02@gmail.com>
2023-03-02 21:03:17 +0000

    Create DownloadOrdBtn component

---
+++ vue-app-v1/src/components/DownloadOrdBtn.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-btn icon="mdi-arrow-down" color="teal-lighten-3" variant="tonal"></v-btn>
+</template>
+
+<script>
+    export default {
+        name: "DownloadOrdBtn",
+        data() {
+            return {
+            }
+        },
+        methods: {
+        }
+    }
+</script>
