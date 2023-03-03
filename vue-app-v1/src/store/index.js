18d7df1de0b46d148f5bfbd05ef6acc2d8468917
Hamiruton <axelhamilton02@gmail.com>
2023-03-03 12:52:54 +0000

    Define global state

--- vue-app-v1/src/store/index.js
+++ vue-app-v1/src/store/index.js
@@ -1,14 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export default createStore ({
+	state: {
+		token: localStorage.getItem('token'),
+	},
+	getters: {
+		user: () => (localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null),
+		token: () => localStorage.getItem('token'),
+	},
+	mutations: {},
+	actions: {
+		login(store, { user, token }) {
+			localStorage.setItem('user', JSON.stringify(user));
+			localStorage.setItem('token', token);
+			window.location.reload()
+		},
+		logout() {
+			localStorage.clear();
+			window.location.reload();
+		},
+	},
+});
