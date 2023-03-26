44b2c7a0b9f0217b2c845cfa50543370c29200e9
Hamiruton <axelhamilton02@gmail.com>
2023-03-26 01:43:05 +0000

    Add colors for app

--- pharma-app/tailwind.config.js
+++ pharma-app/tailwind.config.js
@@ -5,7 +5,43 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: '#eeeeee',
+        primary: {
+          normal: '#80cbc4',
+          shades: {
+            100: '#73b7b0',
+            200: '#66a29d',
+            300: '#5a8e89',
+            400: '#4d7a76',
+            500: '#406662',
+            600: '#33514e',
+            700: '#263d3b',
+          },
+          tints: {
+            100: '#8dd0ca',
+            200: '#99d5d0',
+            300: '#a6dbd6',
+            400: '#b3e0dc',
+            500: '#c0e5e2',
+            600: '#cceae7',
+            700: '#d9efed',
+          }
+        },
+        secondary: {
+          normal: '#004d40',
+          tints: {
+            100: '#1a5f53',
+            200: '#337166',
+            300: '#4d8279',
+            400: '#66948c',
+          }
+        },
+        error: '#b00020',
+        valid: '#62b965', 
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('rippleui')],
 }
